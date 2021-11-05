afe751ed53f223d5f28333d17d19bb7f19448411
abdoxfox <46646744+abdoxfox@users.noreply.github.com>
2021-11-05 16:20:45 +0100

    Update cloudflare_ipranges_scan.py

--- cloudflare_ipranges_scan.py
+++ cloudflare_ipranges_scan.py
@@ -1,8 +1,6 @@
 
 import ipcalc
-import os,platform
-import socket,ssl,random
-import certifi
+import socket,random,re
 
 
 bg=''
@@ -17,8 +15,12 @@ print(O+'''
 \tWEBSOCKET SCANNER
 \tBy : ABDOXFOX
 '''+GR)
-port=input('Enter port number : ')
-ranges = '''104.28.21.0/24
+
+ranges = '''107.154.114.0/24
+196.200.152.5/24
+104.17.208.0/24
+104.17.209.0/24
+104.28.21.0/24
 104.16.116.0/24
 104.24.0.0/14
 103.21.244.0/22
@@ -40,12 +42,6 @@ ranges = '''104.28.21.0/24
 172.68.0.0/13
 172.69.0.0/13
 172.70.0.0/13'''
-connmethod=int(input('''
-Enter your scan method number:
-1-with ssl
-2-without ssl
-: '''))
-wshost = input ('Enter websocket host pointed to cloudflare dns : ')
 dict={}
 print(G+'List of ip ranges :'+GR)
 for k,v in enumerate(ranges.split('\n')):
@@ -54,64 +50,30 @@ for k,v in enumerate(ranges.split('\n')):
 	dict[k]=v
 print(GR)
 
+
 choose = input('enter range number : '.title())
 
-def sock(payload,domain,bughost,port):
-	dest=f'{bughost}:{port}'
-	
-	s=socket.socket(socket.AF_INET,socket.SOCK_STREAM,)
-	s.settimeout(2)
-	dest_host=dest.split(':')[0]	
-	dest_port=int(dest.split(':')[1])
-
+def scanner(host):
+	sock=socket.socket()
+	sock.settimeout(2)
 	try:
-		s.connect((dest_host,dest_port))
-		if connmethod == 1:
-			print(f'{O}SSL handshake with {G}{domain}{GR}	')
-			context = ssl.SSLContext(ssl.PROTOCOL_TLS)
-			context.verify_mode  = ssl.CERT_REQUIRED
-			context.load_verify_locations(
-
-        cafile=os.path.relpath(certifi.where()),
-
-        capath=None,
-
-        cadata=None)
-		#	print(context)
-			s = context.wrap_socket(s,server_hostname=domain,do_handshake_on_connect=True)
-			print(f'{O}Protocol :{G}{s.version()}\n{O}Ciphersuite :{G} {s.cipher()[0]}\n{O}Peerprincipal:{G} C={s.getpeercert()["subject"][0][0][1]} , ST={s.getpeercert()["subject"][1][0][1]} , L={s.getpeercert()["subject"][2][0][1]} , O={s.getpeercert()["subject"][3][0][1]} , CN={s.getpeercert()["subject"][4][0][1]}  {GR}')
-		print(f'{R}Payload : {O}{payload}{GR}')
-		s.send(payload.encode())
-		status = s.recv(1024).decode().split('\n')[0]
-		if status[:12] == 'HTTP/1.1 101':
-			print(f'{G}response : {status}{GR}')
-		else:
-			print(f'{R}response : {status}{GR}')
-		s.send(b'HTTP/1.1 200 Connection Established\r\n\r\n')
-		st=s.recv(30).decode('utf-8','ignore')
-		if st[:4]=='SSH-':
-			print(f'{G}status : {st}{GR}')
-			save(payload,bughost)
-		else:
-			print(f'{R}status : {st}{GR}')
-	except Exception as e:
-		if str(e)=='_ssl.c:1091: The handshake operation timed out':
-			print(R+'CipherSuite: SSL_NULL_WITH_NULL_NULL'+GR)
-		else:
-			print(R+'Error : '+str(e.args)+GR)
-	print('----------------------------------------------')
-		
+		sock.connect((str(host),80))
+		payload=f'GET / HTTP/1.1\r\nHost: {host}\r\n\r\n'
 	
+		sock.send(payload.encode())
+		response=sock.recv(1024).decode('utf-8','ignore')
+		for data in response.split('\r\n'):
+			data=data.split(':')
+			if re.match(r'HTTP/\d(\.\d)?' ,data[0]):
+				print(f'response status : {O}{data[0]}{GR}')
+			if data[0]=='Server':
+				try:
+					if data[1] ==' cloudflare':
+						input(f'{G}server : {data[1]}\nFound working ip press any to continue...')
+				except Exception as e:
+					print(e)
+	except Exception as e:print(e)
 	
-def paylgen(bughost,domain,port):
-		
-		payload =f'GET / HTTP/1.1\r\nHost: {domain}\r\nUpgrade: websocket\nConnection: upgrade\r\nProxy-connection: keep-alive\r\n\r\n'
-		sock(payload,domain,bughost,port)
-def save(payload,bughost):
-	file = open('out.txt','a')
-	file.write(f'payload :{payload.encode()}\nbughost:{bughost}\n')
-	file.write('-------------------------------------------\n')
-
 iprange=[]
 cidr=dict[int(choose)]	
 for ip in ipcalc.Network(cidr):
@@ -119,7 +81,8 @@ for ip in ipcalc.Network(cidr):
 for index in range(len(iprange)):
 			try:
 				print(f"{R}[INFO] Probing... ({index + 1}/{len(iprange)}) [{iprange[index]}]{GR}")
-				paylgen(iprange[index],wshost,port)
+				scanner(iprange[index])
 			except KeyboardInterrupt:
 				print(f'{R}Scan aborted by user!{GR}')
 				break
+
